c57d3712f2614d15250082791bde6640f58c6480
abdul <abdulqadir4344@gmail.com>
2023-08-24 18:35:49 +0530

    first commit on 24-08-2023

---
+++ store/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+from .views.home import Index
+from .views.login import Login
+from .views.signup import Signup
+
+urlpatterns = [
+    path('',Index.as_view() , name = 'homepage'),
+    path('Signup',Signup.as_view(), name='signup'),
+    path('login',Login.as_view() , name='login'),
+
+]
