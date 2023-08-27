6e4ddf9867401d7f7194b3990fd59c26b71fd9a8
abdul <abdulqadir4344@gmail.com>
2023-08-27 12:29:09 +0530

    third commit on 27-08-2023

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-5ogk-0!6n@$(^v*-9@7z0zr7&o$*76u06ayt)3hxx(7dswwjb1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
 
 STATICFILES_DIRS = [
     BASE_DIR /"static"
