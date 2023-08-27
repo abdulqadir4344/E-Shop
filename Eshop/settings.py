8e29768602db5c46930fc7a2f67b2cebee67601f
abdul <abdulqadir4344@gmail.com>
2023-08-27 13:09:33 +0530

    fourth commit on 27-08-2023

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR , "staticfiles")
 
 STATICFILES_DIRS = [
-    BASE_DIR /"static"
+    os.path.join(BASE_DIR , "static"),
 ]
 
 MEDIA_URL = '/image/download/'
