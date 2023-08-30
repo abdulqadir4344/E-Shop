beef72b7dc35b5dfab68f8b927b8129597ca9879
abdul <abdulqadir4344@gmail.com>
2023-08-30 17:44:01 +0530

    30-08-2023

--- store/admin.py
+++ store/admin.py
@@ -3,18 +3,25 @@ from .models.product import Product
 from .models.category import Category
 from .models.customer import Customer
 from .models.orders import Order
+from django.views.decorators.csrf import csrf_exempt
 
-
-class AdminProduct(admin.ModelAdmin):
+@csrf_exempt
+def AdminProduct(ModelAdmin):
     list_display = ['name','price','category']
 
 
-class AdminCategory(admin.ModelAdmin):
+
+
+# class AdminProduct(admin.ModelAdmin):
+#     list_display = ['name','price','category']
+
+@csrf_exempt
+def AdminCategory(ModelAdmin):
     list_display = ['name']   
 
 # Register your models here.
 
-admin.site.register(Product , AdminProduct)
-admin.site.register(Category , AdminCategory)
+admin.site.register(Product)
+admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Order)
